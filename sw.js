ce48e0ce798da3390a6e2c8ae093741800ec0065
kreativmark888 <92460032+kreativmark888@users.noreply.github.com>
2025-02-03 07:18:15 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "ghl-pwa-cache-v1";
+const urlsToCache = [
+    "/",
+    "/index.html",
+    "/pwa-icon-192.png",
+    "/pwa-icon-512.png"
+];
+
+// Install Service Worker
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Activate Service Worker
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch Requests
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
